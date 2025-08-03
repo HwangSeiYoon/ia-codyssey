dfa00a8c3e5e3bc22ab3c219ec3d8e90d8cd91a7
HwangSeiYoon <seuni362@gmail.com>
2025-08-03 20:24:40 +0900

    up

--- course5/question5/david/app.py
+++ course5/question5/david/app.py
@@ -20,5 +20,9 @@ def menu():
 def test1():
     return render_template("test1.html")
 
+@app.route("/test2")
+def test1():
+    return render_template("test2.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
