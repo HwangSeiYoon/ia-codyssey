28cf408e90bed0061a86f8c406032d663ff86702
seuni362 <seuni362@gmail.com>
2025-08-03 08:50:49 +0000

    Feat: 메뉴 화면추가
    - /menu 라우트 추가
    - 메뉴 페이지 템플릿(menu.html)에 버튼 3개 추가
    
    Resolves: #1

--- course5/question5/david/app.py
+++ course5/question5/david/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") 
 def hello_world():
     return "Hello, DevOps!"
+
+@app.route("/menu") 
+def menu():
+    return render_template("menu.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
